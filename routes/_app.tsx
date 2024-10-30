0c0240c8674ebe6c137c00c022e40c1f838fa71f
torresposso <erictorres.co@gmail.com>
2024-10-29 22:04:53 -0500

    add html description

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,11 +2,15 @@ import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
   return (
     <html>
-      <head>
+      <head lang="ES_CO">
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#2196F3"></meta>
-        <title>mundohelado</title>
+        <title>Mundo Helado: Cada bocado, una experiencia única</title>
+        <meta
+          name="description"
+          content="Descubre el mundo del helado en Mundo Helado. Cada bocado una experiencia única "
+        />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
